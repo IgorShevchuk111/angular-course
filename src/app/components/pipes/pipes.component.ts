0f7806e7310b2466f521ffb5aa539b01ac092fcf
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-03-01 11:23:51 +0000

    Pipe: Date, String, Number, Json

--- src/app/components/pipes/pipes.component.ts
+++ src/app/components/pipes/pipes.component.ts
@@ -9,6 +9,23 @@ export class PipesComponent implements OnInit {
 
   now = new Date()
 
+  str:  string = 'Hello world'
+
+  n: number = Math.E
+  number = 0.42
+
+  obj = {
+      a:1,
+      b:{
+        a:2,
+        b:3,
+        c:{
+          a:4,
+          b:5
+          }
+      }
+    }
+
   constructor() { }
 
   ngOnInit(): void {
