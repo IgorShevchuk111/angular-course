a58a4d8704425f051788a92a9c6237f8dbec0fd2
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-03-03 17:31:57 +0000

    Added Controls

--- src/app/components/forms-and-validation/forms-and-validation.component.ts
+++ src/app/components/forms-and-validation/forms-and-validation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms-and-validation',
@@ -8,41 +8,59 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class FormsAndValidationComponent implements OnInit {
 
-  obj = {
-    a:1,
-    b:{
-      c:2,
-      d:3,
-      e:4
-    }
-  }
-
-submit() {
-  console.log('submited',this.form);
-}
-
+  // FORM {}
   form: FormGroup
 
   constructor() { }
 
+  // FORM GROUP
   ngOnInit() {
     this.form = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(5)])
+      password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      address: new FormGroup({
+        country: new FormControl('', [Validators.required]),
+        city: new FormControl('', [Validators.required])
+      }),
+      skills: new FormArray([])
     })
   }
-
+  // SHOW ERROR EMAIL
   getErrorMessageEmail() {
     if (this.form.get('email')?.hasError('required')) {
       return 'You must enter a email';
     }
     return this.form.get('email')?.hasError('email') ? 'Not a valid email' : '';
   }
-
+  // SHOW ERROR PASSWORD
   getErrorMessagePassword() {
     if (this.form.get('password')?.hasError('required')) {
       return 'You must enter a password';
     }
     return this.form.get('password')?.hasError('minlength') ? 'fdf' : '';
+  }
+  // Choose Capital
+  ChooseCapital() {
+    const cityMap = {
+      ua: 'Kyiv',
+      uk: 'London',
+      pl: 'Warsaw'
+    }
+    const cityKey = (this.form.get('address') as FormGroup).get('country')?.value
+    const city = cityMap[cityKey as keyof typeof cityMap];
+    (this.form.get('address') as FormGroup).get('city')?.patchValue(city)
+  }
+  // ADD SKILLS
+  addSkills() {
+    const skill = new FormControl('', [Validators.required]);
+    (this.form.get('skills') as FormArray).push(skill)
+  }
+  // GET  CONTROLS (SKILLS ARRAY)
+  getControls() {
+    return (this.form.get('skills') as FormArray).controls;
+  }
+  // SUBMIT BUTTON
+  submit() {
+    console.log('submited', this.form);
   }
 }
