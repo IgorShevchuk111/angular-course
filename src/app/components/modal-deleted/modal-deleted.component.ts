4848d820efd74a99b1ae6e8070bbd0d92eddc901
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-03-10 11:46:35 +0000

    Added  Dialog

---
+++ src/app/components/modal-deleted/modal-deleted.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-deleted',
+  templateUrl: './modal-deleted.component.html',
+  styleUrls: ['./modal-deleted.component.scss']
+})
+export class ModalDeletedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
